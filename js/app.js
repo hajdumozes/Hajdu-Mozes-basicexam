cedb1474a08d7dcfabc55819048bfc5ba8a885e7
Mózes <hajdu.mozes@gmail.com>
2018-07-08 10:28:51 +0200

    Minor progress

--- js/app.js
+++ js/app.js
@@ -1,9 +1,25 @@
-// ide deklaráljátok a függvényeket.
+function sortByPricesAsc(userDatas) {
+  var pricesAsc = userDatas.slice();
+  var change;
+  var i = pricesAsc.length;
+  while (i > 0) {
+    change = 0;
+    for (var j = 0; j < i - 1; j++) {
+      if (pricesAsc[j].cost_in_credits < pricesAsc[j + 1].cost_in_credits) {
+        [pricesAsc[j], pricesAsc[j + 1]] = [pricesAsc[j + 1], pricesAsc[j]];
+        change = j;
+      }
+    }
+    i = change;
+  }
+  return pricesAsc;
+}
 
 function getData(url, callbackFunc) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
+    // ? ide belenyúltam az ESLINT miatt, 2 == helyett 3 === lett.
+    if (this.readyState === 4 && this.status === 200) {
       callbackFunc(this);
     }
   };
@@ -15,6 +31,7 @@ function successAjax(xhttp) {
   // Innen lesz elérhető a JSON file tartalma, tehát az adatok amikkel dolgoznod kell
   var userDatas = JSON.parse(xhttp.responseText);
   // Innen lehet hívni.
-  console.log(userDatas);
+  var pricesAsc = sortByPricesAsc(userDatas);
+  console.log(pricesAsc);
 }
 getData('/json/spaceships.json', successAjax);
